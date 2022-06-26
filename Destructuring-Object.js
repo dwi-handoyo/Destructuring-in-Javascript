1a0b4c9e6ca84f08f6c70b385acdb38257cf34ea
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-06-26 20:27:08 +0700

    Create Destructuring-Object.js

---
+++ Destructuring-Object.js
@@ -0,0 +1,11 @@
+
+// Destructuring Object - by using {}
+
+const mhs = {
+  nama: 'Godam Best',
+  umur: 23
+}
+
+const {nama, umur} = mhs;
+console.log(nama); // output: Godam Best
+console.log(umur); // output: 23
