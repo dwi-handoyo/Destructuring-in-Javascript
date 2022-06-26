4d4d004053029f17272e809a25ea0be7f580e7fa
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-06-26 20:19:35 +0700

    Create Swap-Items.js

---
+++ Swap-Items.js
@@ -0,0 +1,11 @@
+
+// Swap Items
+
+let a = 1;
+let b = 2;
+console.log(a); // output: 1
+console.log(b); // output: 2 
+
+[a, b] = [b, a];
+console.log(a);  // output: 2
+console.log(b);  // output: 1
