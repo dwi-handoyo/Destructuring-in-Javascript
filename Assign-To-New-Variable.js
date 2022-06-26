6f227ebaccd856a8f2011fb4feb8609dba3a1bf9
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-06-26 20:34:38 +0700

    Create Assign-To-New-Variable.js

---
+++ Assign-To-New-Variable.js
@@ -0,0 +1,15 @@
+
+// Assignment To A New Variable
+
+const mhs = {
+  nama: 'Godam Perkasa',
+  umur: 33
+}
+
+const {nama, umur} = mhs;
+console.log(nama);
+
+const {nama: n, umur: u} = mhs;
+console.log(u); // Output: 33
+
+
