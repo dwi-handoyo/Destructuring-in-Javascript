8a98058b6cfd1e070d9bdb2276a310f63b5dfdcd
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-06-27 06:33:00 +0700

    Update Assign-To-New-Variable.js

--- Assign-To-New-Variable.js
+++ Assign-To-New-Variable.js
@@ -1,15 +1,23 @@
 
 // Assignment To A New Variable
 
+// Object:
 const mhs = {
-  nama: 'Godam Perkasa',
+  nama: 'Godam Hero',
   umur: 33
 }
 
+// Destructuring:
 const {nama, umur} = mhs;
-console.log(nama);
+console.log(nama); // output: 33
+console.log(umur); // output: Godam Hero
 
+// Assign to new variable:
 const {nama: n, umur: u} = mhs;
+
+// Result:
 console.log(u); // Output: 33
+console.log(n); // Output: Godam Hero
+
 
 
