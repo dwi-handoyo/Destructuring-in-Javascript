13504cc0afa55e419e52c26ce4576a2630dcb021
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-06-27 06:43:56 +0700

    Update Assign-To-New-Variable.js

--- Assign-To-New-Variable.js
+++ Assign-To-New-Variable.js
@@ -18,6 +18,13 @@ const {nama: n, umur: u} = mhs;
 // Result:
 console.log(u); // Output: 33
 console.log(n); // Output: Godam Hero
+console.log(mhs);
+// Output:
+// Object idak berubah setelah destructuring (Object is not changed after destructuring)
+// {
+//  nama: "Godam Hero",
+//  umur: 33
+// }
 
 
 
