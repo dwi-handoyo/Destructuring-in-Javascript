6884858fcdfce9c133ed69e0c500fd70bfe2e1b6
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-06-27 07:11:13 +0700

    Update Rest-Parameter.js

--- Rest-Parameter.js
+++ Rest-Parameter.js
@@ -1,15 +1,17 @@
 
+// Rest Parameter in Array (...something)
 
-// Rest Parameter (...something)
-
+// Destructuring Array #1 
 const [e, f] = [1, 2, 3];
-console.log(e); // 1
-//console.log(g);  // Error
+console.log(e); // output: 1
+//console.log(g);  // output: Error
 
+// Destructuring Array #2
 const [h, i, j] = [1, 2, 3, 4, 5];
 console.log(h); // output: 1
 console.log(j); // output: 3
 
+// Destructuring Array #3
 const [k, ...values] = [1, 2, 3, 4, 5, 6];
 console.log(k);  // output: 1
 console.log(values); // output: [2, 3, 4, 5, 6] 
